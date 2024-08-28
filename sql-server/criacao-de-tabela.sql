b5b3162748ce0ccaa75d787a9403fb4a7a5942af
Lucas Wyllamys <lucas.wyllamys@outlook.com>
2024-08-28 11:42:59 -0300

    28.08.2024-11:42

---
+++ sql-server/criacao-de-tabela.sql
@@ -0,0 +1,9 @@
+-- Criação de tabela
+CREATE TABLE dbo.MunicipiosUTDs40 MunicipiosUTDs40 (
+	ID INT IDENTITY(1,1) PRIMARY KEY,
+	Municipio VARCHAR(255),
+	Bairro VARCHAR(255),
+	UTD40 VARCHAR(255)
+)
+
+
