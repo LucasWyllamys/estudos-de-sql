061dacd106a8aac1c3d7ce8f035a811c49b03794
LUCAS WYLLAMYS CARMO DA SILVA <127877422+LucasWyllamys@users.noreply.github.com>
2024-10-10 19:15:37 -0300

    10.10.2024-19:15

---
+++ sql-server/remover-todos-os-dados-existentes-na-tabela.sql
@@ -0,0 +1,7 @@
+-- Remover todos os dados existentes na tabela de uma vez
+
+-- Usando TRANCATE: Remove todos os dados existentes na tabela de uma vez e rápido!
+TRUNCATE TABLE DeptoGrandesClientes.dbo.MontantesUsoContratados;
+
+-- Usando DELETE: Remove todos os dados existentes na tabela de uma vez e pode ser usado com uma cláusula WHERE para remover linhas específicas.
+DELETE FROM DeptoGrandesClientes.dbo.MontantesUsoContratados;
